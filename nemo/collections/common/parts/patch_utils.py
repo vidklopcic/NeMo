86201565be22c78723f732bfd36ff93bdb7afdc4
vidklopcic <klopcic.vid@gmail.com>
2021-10-19 11:43:36 +0200

    prepare patch for istft and stft

--- nemo/collections/common/parts/patch_utils.py
+++ nemo/collections/common/parts/patch_utils.py
@@ -18,6 +18,23 @@ import torch
 from packaging import version
 
 from nemo.utils import logging
+from typing import (
+    Tuple, Optional, Union, Any, Sequence, TYPE_CHECKING
+)
+
+import torch
+import torch.nn.functional as F
+from torch.types import _size
+from torch._lowrank import svd_lowrank, pca_lowrank
+from torch.overrides import (
+    has_torch_function, has_torch_function_unary, has_torch_function_variadic,
+    handle_torch_function)
+from torch._jit_internal import boolean_dispatch, List
+from torch._jit_internal import _overload as overload
+from torch._autograd_functions import _LU
+
+Tensor = torch.Tensor
+from torch import _VF
 
 # Library version globals
 TORCH_VERSION = None
@@ -25,16 +42,16 @@ TORCH_VERSION_MIN = version.Version('1.7')
 
 
 def stft_patch(
-    input: torch.Tensor,
-    n_fft: int,
-    hop_length: Optional[int] = None,
-    win_length: Optional[int] = None,
-    window: Optional[torch.Tensor] = None,
-    center: bool = True,
-    pad_mode: str = 'reflect',
-    normalized: bool = False,
-    onesided: Optional[bool] = None,
-    return_complex: Optional[bool] = None,
+        input: torch.Tensor,
+        n_fft: int,
+        hop_length: Optional[int] = None,
+        win_length: Optional[int] = None,
+        window: Optional[torch.Tensor] = None,
+        center: bool = True,
+        pad_mode: str = 'reflect',
+        normalized: bool = False,
+        onesided: Optional[bool] = None,
+        return_complex: Optional[bool] = None,
 ):
     """
     Patch over torch.stft for PyTorch <= 1.6.
@@ -52,7 +69,7 @@ def stft_patch(
         )
 
     if TORCH_VERSION < TORCH_VERSION_MIN:
-        return torch.stft(
+        return stft(
             input=input,
             n_fft=n_fft,
             hop_length=hop_length,
@@ -64,7 +81,7 @@ def stft_patch(
             onesided=True,
         )
     else:
-        return torch.stft(
+        return stft(
             input=input,
             n_fft=n_fft,
             hop_length=hop_length,
@@ -79,16 +96,16 @@ def stft_patch(
 
 
 def istft_patch(
-    input: torch.Tensor,
-    n_fft: int,
-    hop_length: Optional[int] = None,
-    win_length: Optional[int] = None,
-    window: Optional[torch.Tensor] = None,
-    center: bool = True,
-    normalized: bool = False,
-    onesided: Optional[bool] = None,
-    length: int = None,
-    return_complex: Optional[bool] = False,
+        input: torch.Tensor,
+        n_fft: int,
+        hop_length: Optional[int] = None,
+        win_length: Optional[int] = None,
+        window: Optional[torch.Tensor] = None,
+        center: bool = True,
+        normalized: bool = False,
+        onesided: Optional[bool] = None,
+        length: int = None,
+        return_complex: Optional[bool] = False,
 ):
     """
     Patch over torch.stft for PyTorch <= 1.6.
@@ -106,7 +123,7 @@ def istft_patch(
         )
 
     if TORCH_VERSION < TORCH_VERSION_MIN:
-        return torch.istft(
+        return istft(
             input=input,
             n_fft=n_fft,
             hop_length=hop_length,
@@ -118,7 +135,7 @@ def istft_patch(
             onesided=True,
         )
     else:
-        return torch.istft(
+        return istft(
             input=input,
             n_fft=n_fft,
             hop_length=hop_length,
@@ -130,3 +147,42 @@ def istft_patch(
             onesided=onesided,
             return_complex=return_complex,
         )
+
+
+def stft(input: Tensor, n_fft: int, hop_length: Optional[int] = None,
+         win_length: Optional[int] = None, window: Optional[Tensor] = None,
+         center: bool = True, pad_mode: str = 'reflect', normalized: bool = False,
+         onesided: Optional[bool] = None,
+         return_complex: Optional[bool] = None) -> Tensor:
+    print('HERE: stft')
+    if has_torch_function_unary(input):
+        return handle_torch_function(
+            stft, (input,), input, n_fft, hop_length=hop_length, win_length=win_length,
+            window=window, center=center, pad_mode=pad_mode, normalized=normalized,
+            onesided=onesided, return_complex=return_complex)
+    # TODO: after having proper ways to map Python strings to ATen Enum, move
+    #       this and F.pad to ATen.
+    if center:
+        signal_dim = input.dim()
+        extended_shape = [1] * (3 - signal_dim) + list(input.size())
+        pad = int(n_fft // 2)
+        input = F.pad(input.view(extended_shape), [pad, pad], pad_mode)
+        input = input.view(input.shape[-signal_dim:])
+    return _VF.stft(input, n_fft, hop_length, win_length, window,  # type: ignore[attr-defined]
+                    normalized, onesided, return_complex)
+
+
+def istft(input: Tensor, n_fft: int, hop_length: Optional[int] = None,
+          win_length: Optional[int] = None, window: Optional[Tensor] = None,
+          center: bool = True, normalized: bool = False,
+          onesided: Optional[bool] = None, length: Optional[int] = None,
+          return_complex: bool = False) -> Tensor:
+    print('HERE: istft')
+    if has_torch_function_unary(input):
+        return handle_torch_function(
+            istft, (input,), input, n_fft, hop_length=hop_length, win_length=win_length,
+            window=window, center=center, normalized=normalized, onesided=onesided,
+            length=length, return_complex=return_complex)
+
+    return _VF.istft(input, n_fft, hop_length, win_length, window, center,  # type: ignore[attr-defined]
+                     normalized, onesided, length, return_complex)
