6e1acfc6b9af5e598814b92f9258190ba108592e
vidklopcic <klopcic.vid@gmail.com>
2021-10-25 13:25:38 +0200

    unfold patch

--- nemo/collections/common/parts/patch_utils.py
+++ nemo/collections/common/parts/patch_utils.py
@@ -163,15 +163,15 @@ def stft(input: Tensor, n_fft: int, hop_length: Optional[int] = None,
     # print('win_length:', win_length)
     # ---->
     # size: torch.Size([16, 206720])
-    # n_fft: 512
-    # hop_length: 160
-    # win_length: 400
+    # n_fft=512
+    # hop_length=160
+    # win_length=400
     # n_fft: 512
     # hop_length: 160
     # win_length: 400
     if has_torch_function_unary(input):
         return handle_torch_function(
-            stft, (input,), input, n_fft, hop_length=win_length or n_fft, win_length=win_length,
+            stft, (input,), input, n_fft, hop_length=hop_length, win_length=win_length,
             window=window, center=center, pad_mode=pad_mode, normalized=normalized,
             onesided=onesided, return_complex=return_complex)
     # TODO: after having proper ways to map Python strings to ATen Enum, move
@@ -182,8 +182,9 @@ def stft(input: Tensor, n_fft: int, hop_length: Optional[int] = None,
         pad = int(n_fft // 2)
         input = F.pad(input.view(extended_shape), [pad, pad], pad_mode)
         input = input.view(input.shape[-signal_dim:])
-    input = input.unfold(-1, win_length or n_fft, hop_length).flatten(-2)
-    return _VF.stft(input, n_fft, win_length or n_fft, win_length, window,  # type: ignore[attr-defined]
+    input = input.unfold(-1, n_fft, hop_length).flatten(-2)
+    hop_length = n_fft
+    return _VF.stft(input, n_fft, hop_length, win_length, window,  # type: ignore[attr-defined]
                     normalized, onesided, return_complex)
 
 
@@ -198,6 +199,7 @@ def istft(input: Tensor, n_fft: int, hop_length: Optional[int] = None,
             window=window, center=center, normalized=normalized, onesided=onesided,
             length=length, return_complex=return_complex)
 
-    input = input.unfold(-1, win_length or n_fft, hop_length).flatten(-2)
-    return _VF.istft(input, n_fft, win_length or n_fft, win_length, window, center,  # type: ignore[attr-defined]
+    input = input.unfold(-1, n_fft, hop_length).flatten(-2)
+    hop_length = n_fft
+    return _VF.istft(input, n_fft, hop_length, win_length, window, center,  # type: ignore[attr-defined]
                      normalized, onesided, length, return_complex)
